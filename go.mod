ddbfd773a4dce40a1b4dd14da10989949537a2a7
Emmanuel Fontan <fontanemmanuel@gmail.com>
2021-05-28 11:29:53 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/efontan/buenbit-receipts-to-csv
+
+go 1.15
+
+require (
+	code.sajari.com/docconv v1.1.0
+	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
+	github.com/advancedlogic/GoOse v0.0.0-20200830213114-1225d531e0ad // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/unidoc/unipdf/v3 v3.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"code.sajari.com/docconv"
+)
+
+type tableData struct {
+	Operation      string
+	CryptoAmount   string
+	InvestedAmount string
+	ReceivedAmount string
+	Price          string
+	Date           string
+}
+
+func (td *tableData) ToSlice() []string {
+	data := make([]string, 0)
+	data = append(data, td.Operation)
+	data = append(data, td.CryptoAmount)
+	data = append(data, td.InvestedAmount)
+	data = append(data, td.ReceivedAmount)
+	data = append(data, td.Price)
+	data = append(data, td.Date)
+	return data
+}
+
+func main() {
+	sum := 0
+
+	// create csv output file
+	file, err := os.Create("result.csv")
+	if err != nil {
+		log.Fatal("creating file", err)
+	}
+	defer file.Close()
+	csvWriter := csv.NewWriter(file)
+	defer csvWriter.Flush()
+
+	// write header
+	if err := csvWriter.Write([]string{"Operacion", "Cantidad", "Monto invertido", "Monto recibido", "Cotizacion", "Fecha"}); err != nil {
+		log.Fatal("writing to file", err)
+	}
+
+	err = filepath.Walk("files", func(path string, info os.FileInfo, err error) error {
+		sum++
+		if info.IsDir() || info.Name() == ".gitkeep" {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("reading file %s\n", path)
+		data := &tableData{}
+
+		res, err := docconv.ConvertPath(path)
+		if err != nil {
+			panic(err)
+		}
+
+		lines := strings.Split(res.Body, "\n")
+		spplitedValues := strings.Split(lines[8], " ")
+
+		operation := spplitedValues[0]
+		data.Operation = operation + " " + spplitedValues[1]
+		data.CryptoAmount = spplitedValues[3]
+
+		var amount, price string
+		if len(spplitedValues) == 8 {
+			price = spplitedValues[4] + " " + spplitedValues[5]
+			amount = spplitedValues[6] + " " + spplitedValues[7]
+		} else if len(spplitedValues) == 6 {
+			price = spplitedValues[4] + " " + spplitedValues[5]
+			spplitedValues = strings.Split(lines[9], " ")
+			amount = spplitedValues[0] + " " + spplitedValues[1]
+		} else if len(spplitedValues) == 4 {
+			spplitedValues = strings.Split(lines[9], " ")
+			price = spplitedValues[0] + " " + spplitedValues[1]
+			amount = spplitedValues[2] + " " + spplitedValues[3]
+		}
+
+		if operation == "Compra" {
+			data.InvestedAmount = amount
+			data.ReceivedAmount = ""
+		} else if operation == "Venta" {
+			data.InvestedAmount = ""
+			data.ReceivedAmount = amount
+		} else {
+			return fmt.Errorf("unkown operation: %s", operation)
+		}
+		data.Price = price
+		date := strings.Split(lines[7], " ")[0]
+		data.Date = date
+
+		// write data
+		values := data.ToSlice()
+		if err := csvWriter.Write(values); err != nil {
+			log.Fatal("writing to file", err)
+		}
+
+		return nil
+	})
+
+	log.Printf("processed files: %d\n", sum)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
